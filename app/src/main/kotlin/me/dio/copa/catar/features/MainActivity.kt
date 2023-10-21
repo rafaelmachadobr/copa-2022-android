df2cc1f3816313ee214fe46e3fb0e360df6d832f
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-10-20 23:13:27 -0300

    Criado Work Manager para orquestrar notificações push localmente

--- app/src/main/kotlin/me/dio/copa/catar/features/MainActivity.kt
+++ app/src/main/kotlin/me/dio/copa/catar/features/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.viewModels
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import dagger.hilt.android.AndroidEntryPoint
+import me.dio.copa.catar.extensions.observe
+import me.dio.copa.catar.notification.scheduler.extensions.NotificationMatcherWorker
 import me.dio.copa.catar.ui.theme.Copa2022Theme
 
 @AndroidEntryPoint
@@ -16,12 +18,32 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        observeActions()
         setContent {
             Copa2022Theme {
                 val state by viewModel.state.collectAsState()
-                MainScreen(matches = state.matches)
+                MainScreen(matches = state.matches, viewModel::toggleNotification)
             }
         }
     }
+
+    private fun observeActions() {
+        viewModel.action.observe(this) { action ->
+            when (action) {
+                is MainUiAction.MatchesNotFound -> TODO()
+                MainUiAction.Unexpected -> TODO()
+                is MainUiAction.DisableNotification -> NotificationMatcherWorker.cancel(
+                    applicationContext,
+                    action.match
+                )
+
+                is MainUiAction.EnableNotification -> NotificationMatcherWorker.start(
+                    applicationContext,
+                    action.match
+                )
+            }
+        }
+    }
+
 
 }
--- app/src/main/kotlin/me/dio/copa/catar/features/MainViewModel.kt
+++ app/src/main/kotlin/me/dio/copa/catar/features/MainViewModel.kt
@@ -6,8 +6,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import me.dio.copa.catar.core.BaseViewModel
+import me.dio.copa.catar.domain.model.Match
 import me.dio.copa.catar.domain.model.MatchDomain
+import me.dio.copa.catar.domain.usecase.DisableNotificationUseCase
+import me.dio.copa.catar.domain.usecase.EnableNotificationUseCase
 import me.dio.copa.catar.domain.usecase.GetMatchesUseCase
 import me.dio.copa.catar.remote.NotFoundException
 import me.dio.copa.catar.remote.UnexpectedException
@@ -16,6 +20,8 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val getMatchesUseCase: GetMatchesUseCase,
+    private val disableNotificationUseCase: DisableNotificationUseCase,
+    private val enableNotificationUseCase: EnableNotificationUseCase,
 ) : BaseViewModel<MainUiState, MainUiAction>(MainUiState()) {
 
     init {
@@ -23,21 +29,39 @@ class MainViewModel @Inject constructor(
     }
 
     private fun fetchMatches() = viewModelScope.launch {
-        getMatchesUseCase()
-            .flowOn(Dispatchers.Main)
-            .catch {
-                when (it) {
-                    is NotFoundException ->
-                        sendAction(MainUiAction.MatchesNotFound(it.message ?: "Erro sem mensagem"))
+        getMatchesUseCase().flowOn(Dispatchers.Main).catch {
+            when (it) {
+                is NotFoundException -> sendAction(
+                    MainUiAction.MatchesNotFound(
+                        it.message ?: "Erro sem mensagem"
+                    )
+                )
 
-                    is UnexpectedException ->
-                        sendAction(MainUiAction.Unexpected)
-                }
-            }.collect { matches ->
-                setState {
-                    copy(matches = matches)
+                is UnexpectedException -> sendAction(MainUiAction.Unexpected)
+            }
+        }.collect { matches ->
+            setState {
+                copy(matches = matches)
+            }
+        }
+    }
+
+    fun toggleNotification(match: Match) {
+        viewModelScope.launch {
+            runCatching {
+                withContext(Dispatchers.Main) {
+                    val action = if (match.notificationEnabled) {
+                        disableNotificationUseCase(match.id)
+                        MainUiAction.DisableNotification(match)
+                    } else {
+                        enableNotificationUseCase(match.id)
+                        MainUiAction.EnableNotification(match)
+                    }
+
+                    sendAction(action)
                 }
             }
+        }
     }
 }
 
@@ -45,7 +69,9 @@ data class MainUiState(
     val matches: List<MatchDomain> = emptyList()
 )
 
-sealed class MainUiAction {
-    object Unexpected : MainUiAction()
-    data class MatchesNotFound(val message: String) : MainUiAction()
+sealed interface MainUiAction {
+    object Unexpected : MainUiAction
+    data class MatchesNotFound(val message: String) : MainUiAction
+    data class EnableNotification(val match: MatchDomain) : MainUiAction
+    data class DisableNotification(val match: MatchDomain) : MainUiAction
 }
